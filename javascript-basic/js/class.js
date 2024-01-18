ac142b20d6d9e47fad15d3bc148fbfd4a9ecb5bf
Miyu1008 <137013919+Miyu1008@users.noreply.github.com>
2024-01-18 12:43:21 +0900

    iroiro

---
+++ javascript-basic/js/class.js
@@ -0,0 +1,26 @@
+ // クラスを定義する
+ class Product {
+   // インスタンス化すると同時に処理を実行（初期化）する
+   constructor(name,price,category) {
+  // インスタンス(オブジェクト)にプロパティを持たせる
+  this.name = 'name';
+  this.price = price;
+  this.category = 'category';
+   }
+ }
+ 
+ // メソッドを定義する
+  describe(){
+    console.log('この商品名は'　+ this.name + 'です。');
+  }
+ // インスタンス化する
+ const shampoo = new Product('シャンプー', 500, '生活雑貨');
+ const coffe = new Product('コーヒー', 1500,'飲料');
+
+
+ // インスタンス(オブジェクト)の値を出力する
+ console.log(shampoo);
+ console.log(coffee);
+
+ // メソッドを呼び出す（実行する）
+ shampoo.describe();
